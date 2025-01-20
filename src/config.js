ac6cd2608e5907bee76b6cc5b0ad1efcce34d415
beeplovsharma <beeplovsharma@gmail.com>
2025-01-20 09:57:33 +0530

    final code

---
+++ src/config.js
@@ -0,0 +1,80 @@
+export const config = {
+  title: "Full Stack Developer",
+  sections: ["home", "skills", "projects", "contact"],
+  home: {
+    title: "BEEPLOV",
+    subtitle: "SHARMA",
+  },
+  skills: [
+    {
+      name: "React",
+      icon: "icons/react-native.png",
+      level: 80,
+    },
+    {
+      name: "JavaScript",
+      icon: "icons/javascript.png",
+      level: 80,
+    },
+    {
+      name: "Three.js",
+      icon: "icons/threejs.png",
+      level: 70,
+    },
+    // {
+    //   name: "Blender",
+    //   icon: "icons/blender-3d.png",
+    //   level: 50,
+    // },
+    // {
+    //   name: "French",
+    //   icon: "icons/france.png",
+    //   level: 100,
+    // },
+    // {
+    //   name: "English",
+    //   icon: "icons/usa.png",
+    //   level: 90,
+    // },
+    // {
+    //   name: "Japanese",
+    //   icon: "icons/japan.png",
+    //   level: 30,
+    // },
+  ],
+  projects: [
+    {
+      name: "Marshmallow Design",
+      description: "HTML | CSS | JS | GSAP | ScrollTrigger",
+      image: "projects/project1.jpg",
+      link: "https://lucky-halva-68638e.netlify.app/",
+    },
+    {
+      name: "Apple Clone",
+      description: "GSAP | Canvas",
+      image: "projects/project2.jpg",
+      link: "https://preeminent-piroshki-4cb19c.netlify.app/",
+    },
+    {
+      name: "RedBull",
+      description: "Redesigned RedBull using React | ThreeJS",
+      image: "projects/project3.jpg",
+      link: "https://project-redbull.vercel.app/",
+    },
+    {
+      name: "Zelt Clone",
+      description: "GSAP | Canvas",
+      image: "projects/project4.jpg",
+      link: "https://cool-licorice-7bd1ff.netlify.app/",
+    },
+  ],
+  contact: {
+    name: "Beeplov Sharma",
+    address: "Bangalore",
+    socials: {
+      linkedin: "https://www.linkedin.com/in/beeplov-sharma-017a22237/",
+      twitter: "https://twitter.com/wawasensei",
+    },
+    mail: "contact@wawasensei.dev",
+  },
+};
